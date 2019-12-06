611ab3b2570dd43caa5657b3957c66c1f7a03e50
CCutecute <897996795@qq.com>
2019-12-06 17:42:08 +0800

    change csv to parquet

---
+++ src/main/scala/dmp/Entry/Log.scala
@@ -0,0 +1,103 @@
+package dmp.Entry
+
+/**
+  * Description:
+  * Copyright (c),2019,JingxuanYan 
+  * This program is protected by copyright laws. 
+  *
+  * @author 闫敬轩
+  * @date 2019/12/6 17:29
+  * @version 1.0
+  */
+
+/**
+  * 样例类 用于解析日志中的字段
+  */
+case class Log(
+                sessionid: String,
+                advertisersid: Int,
+                adorderid: Int,
+                adcreativeid: Int,
+                adplatformproviderid: Int,
+                sdkversion: String,
+                adplatformkey: String,
+                putinmodeltype: Int,
+                requestmode: Int,
+                adprice: Double,
+                adppprice: Double,
+                requestdate: String,
+                ip: String,
+                appid: String,
+                appname: String,
+                uuid: String,
+                device: String,
+                client: Int,
+                osversion: String,
+                density: String,
+                pw: Int,
+                ph: Int,
+                long_s: String,
+                lat: String,
+                provincename: String,
+                cityname: String,
+                ispid: Int,
+                ispname: String,
+                networkmannerid: Int,
+                networkmannername: String,
+                iseffective: Int,
+                isbilling: Int,
+                adspacetype: Int,
+                adspacetypename: String,
+                devicetype: Int,
+                processnode: Int,
+                apptype: Int,
+                district: String,
+                paymode: Int,
+                isbid: Int,
+                bidprice: Double,
+                winprice: Double,
+                iswin: Int,
+                cur: String,
+                rate: Double,
+                cnywinprice: Double,
+                imei: String,
+                mac: String,
+                idfa: String,
+                openudid: String,
+                androidid: String,
+                rtbprovince: String,
+                rtbcity: String,
+                rtbdistrict: String,
+                rtbstreet: String,
+                storeurl: String,
+                realip: String,
+                isqualityapp: Int,
+                bidfloor: Double,
+                aw: Int,
+                ah: Int,
+                imeimd5: String,
+                macmd5: String,
+                idfamd5: String,
+                openudidmd5: String,
+                androididmd5: String,
+                imeisha1: String,
+                macsha1: String,
+                idfasha1: String,
+                openudidsha1: String,
+                androididsha1: String,
+                uuidunknow: String,
+                userid: String,
+                iptype: Int,
+                initbidprice: Double,
+                adpayment: Double,
+                agentrate: Double,
+                lomarkrate: Double,
+                adxrate: Double,
+                title: String,
+                keywords: String,
+                tagid: String,
+                callbackdate: String,
+                channelid: String,
+                mediatype: Int
+              )
+
