c8b7c860fe02f53e73e94154bb92551ae64e63ce
CCutecute <897996795@qq.com>
2019-12-07 17:26:17 +0800

    DBUTILES

---
+++ src/main/scala/dmp/util/DBUtils.scala
@@ -0,0 +1,14 @@
+package dmp.util
+
+/**
+  * Description:
+  * Copyright (c),2019,JingxuanYan 
+  * This program is protected by copyright laws. 
+  *
+  * @author 闫敬轩
+  * @date 2019/12/7 17:26
+  * @version 1.0
+  */
+object DBUtils {
+
+}
