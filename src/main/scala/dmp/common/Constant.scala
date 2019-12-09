d9603084f9979bfff17c033cf4261f4362a2ba13
CCutecute <897996795@qq.com>
2019-12-09 19:55:58 +0800

    Constant

--- src/main/scala/dmp/common/Constant.scala
+++ src/main/scala/dmp/common/Constant.scala
@@ -1,5 +1,6 @@
 package dmp.common
 
+import org.apache.log4j.Logger
 import org.apache.spark.sql.SaveMode
 
 /**
@@ -17,15 +18,41 @@ object Constant {
   val APPLICATION_NAME ="DMP"
   val MASTER ="local[2]"
 
-  val LOG_URL = "D://XSQ-BigData-24/项目/DMP项目资料/2016-10-01_06_p1_invalid.1475274123982.log"
-  val LOG_SAVE_URL = "D://XSQ-BigData-24/项目/DMP项目资料/2016-10-01_06_p1_invalid.1475274123982.log.parquet"
+  val LOG_URL = "D://XSQ-BigData-24/项目/DMP项目资料/2016-10-01_06_p1_invalid.1475274123982.log.FINISH"
+  val LOG_SAVE_URL = "D://XSQ-BigData-24/项目/DMP项目资料/2016-10-01_06_p1_invalid.1475274123982.log.FINISH.parquet"
+  val VOLUMN_SAVE_URL = "D://XSQ-BigData-24/项目/DMP项目资料/3.2.0Volumn.log"
+  val APP_DICT_URL = "D:\\XSQ-BigData-24\\项目\\DMP项目资料/app_dict.txt"
+  val STOPWORDS_URL = "D:\\XSQ-BigData-24\\项目\\DMP项目资料/stopwords.txt"
 
   val DB_URL ="jdbc:mysql://127.0.0.1:3306/advertising?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC"
   val DB_USERNAME="root"
   val DB_PASSWORD="123321"
+  val DB_VOLUMN = "volumn"
 
   val SERIALIZER ="org.apache.spark.serializer.KryoSerializer"
   val SAVE_MODE = SaveMode.Overwrite
+
+  val AD_PREFIX = "LN"
+  val APP_PREFIX = "APP"
+  val CHANNEL_PREFIX = "CN"
+  val KEY_WORD_PREFIX = "K"
+  val REGION_PROVINCE_PREFIX = "ZP"
+  val REGION_CITY_PREFIX = "ZC"
+
+  val HBASE_CONNECT_KEY = "hbase.zookeeper.quorum"
+  val HBASE_CONNECT_VALUE = "hadoop04:2181"
+
+  val JEDIS_MAX_IDLE = 100
+  val JEDIS_MAX_TOTAL = 300
+  val JEDIS_MIN_IDLE = 1
+  val JEDIS_TIME_OUT = 30000
+  val JEDIS_HOST = "hadoop04"
+  val JEDIS_PORT = 6379
+  val JEDIS_ON_BORROW = true
+  val JEDIS_TRADING_AREA_TABLE = "tradingArea"
+
+  val GAODE_API_URL = "https://restapi.amap.com/v3/geocode/regeo?output=json&radius=5000&extensions=all"
+  val GAODE_KEY = "aa9aefe13d7fa17a7d84f26fe1bf0003"
 }
 
 /**
