043054d17c1adbaeac5fc692b934e5bdaba59f5e
CCutecute <897996795@qq.com>
2019-12-09 19:55:44 +0800

    final region report

--- src/main/scala/dmp/report/Region.scala
+++ src/main/scala/dmp/report/Region.scala
@@ -22,6 +22,12 @@ object Region {
     val regionDF = spark.sql(
       """
         |select
+        |provincename,cityname,
+        |OriginalRequest,ValidRequest,adRequest,bidsNum,bidsSus,
+        |bidsSus / bidsNum,adDisplayNum,adClickNum,
+        |adDisplayNum / adClickNum,adconsume,adcost
+        |from
+        |(select
         |sum(case when requestmode=1 and processnode>=1 then 1 else 0 end) as OriginalRequest,
         |sum(case when requestmode=1 and processnode>=2 then 1 else 0 end) as ValidRequest,
         |sum(case when requestmode=1 and processnode=3 then 1 else 0 end) as adRequest,
@@ -32,7 +38,7 @@ object Region {
         |sum(case when iseffective=1 and isbilling=1 and iswin=1 then winprice/1000 else 0 end) as adconsume,
         |sum(case when iseffective=1 and isbilling=1 and iswin=1 then adpayment/1000 else 0 end) as adcost
         |from tb_log
-        |group by provincename, cityname
+        |group by provincename, cityname) t
       """.stripMargin)
     regionDF.show()
 
